eec056585d0d8fba1d4d91c12c4fabd6dd4ea69e
Doadaodao <81339151+Doadaodao@users.noreply.github.com>
2025-05-05 22:25:05 +0800

    Q1_fix_torch_load

--- Q1/student_agent.py
+++ Q1/student_agent.py
@@ -22,7 +22,8 @@ class Agent(object):
             PPO_kwargs=self.cfg.PPO_kwargs,
             device=self.cfg.device
         )
-        self.agent.actor.load_state_dict(torch.load(self.cfg.save_path))
+        self.agent.actor.load_state_dict(torch.load(self.cfg.save_path, map_location=torch.device('cpu')))
+
 
     def act(self, observation):
         action = self.agent.policy(observation)
