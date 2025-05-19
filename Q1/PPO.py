dfbb11f314e9c267e56ffebf8f3be7407c39f2da
Doadaodao <81339151+Doadaodao@users.noreply.github.com>
2025-05-19 21:07:12 +0800

    modify PPO

--- Q1/PPO.py
+++ Q1/PPO.py
@@ -1,19 +1,11 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-import numpy as np
-import gymnasium as gym
-import copy
 import random
 from collections import deque
-from tqdm import tqdm
 import typing as typ
 
 class policyNet(nn.Module):
-    """
-    continuity action:
-    normal distribution (mean, std) 
-    """
     def __init__(self, state_dim: int, hidden_layers_dim: typ.List, action_dim: int):
         super(policyNet, self).__init__()
         self.features = nn.ModuleList()
@@ -51,7 +43,6 @@ class valueNet(nn.Module):
             x = layer['linear_activation'](layer['linear'](x))
         return self.head(x)
 
-
 def compute_advantage(gamma, lmbda, td_delta):
     td_delta = td_delta.detach().numpy()
     adv_list = []
@@ -61,7 +52,6 @@ def compute_advantage(gamma, lmbda, td_delta):
         adv_list.append(adv)
     adv_list.reverse()
     return torch.FloatTensor(adv_list)
-
 
 class PPO:
     def __init__(self,
@@ -145,83 +135,3 @@ class replayBuffer:
 
     def sample(self, batch_size: int):
         return random.sample(self.buffer, batch_size)
-
-class Config:
-    num_episode = 1200
-    state_dim = None
-    hidden_layers_dim = [ 64, 64, 64 ]
-    action_dim = 20
-    actor_lr = 1e-4
-    critic_lr = 5e-3
-    PPO_kwargs = {
-        'lmbda': 0.9,
-        'eps': 0.2,
-        'ppo_epochs': 10
-    }
-    gamma = 0.9
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    buffer_size = 100000
-    minimal_size = 1024
-    batch_size = 128
-    save_path = './PPO_Pendulum.pth'
-
-    max_episode_rewards = 260
-    max_episode_steps = 260
-    
-    def __init__(self, env):
-        self.state_dim = env.observation_space.shape[0]
-        try:
-            self.action_dim = env.action_space.n
-        except Exception as e:
-            self.action_dim = env.action_space.shape[0]
-        print(f'device={self.device} | env={str(env)}')
-
-
-def train_agent(env, cfg):
-    ac_agent = PPO(
-        state_dim=cfg.state_dim,
-        hidden_layers_dim=cfg.hidden_layers_dim,
-        action_dim=cfg.action_dim,
-        actor_lr=cfg.actor_lr,
-        critic_lr=cfg.critic_lr,
-        gamma=cfg.gamma,
-        PPO_kwargs=cfg.PPO_kwargs,
-        device=cfg.device
-    )           
-    tq_bar = tqdm(range(cfg.num_episode))
-    rewards_list = []
-    now_reward = 0
-    bf_reward = -np.inf
-    for i in tq_bar:
-        buffer_ = replayBuffer(cfg.buffer_size)
-        tq_bar.set_description(f'Episode [ {i+1} / {cfg.num_episode} ]')    
-        s, _ = env.reset()
-        done = False
-        episode_rewards = 0
-        steps = 0
-        while not done:
-            a = ac_agent.policy(s)
-            n_s, r, done, _, _ = env.step(a)
-            buffer_.add(s, a, r, n_s, done)
-            s = n_s
-            episode_rewards += r
-            steps += 1
-            if (episode_rewards >= cfg.max_episode_rewards) or (steps >= cfg.max_episode_steps):
-                break
-
-        ac_agent.update(buffer_.buffer)
-        rewards_list.append(episode_rewards)
-        now_reward = np.mean(rewards_list[-10:])
-        if bf_reward < now_reward:
-            torch.save(ac_agent.actor.state_dict(), cfg.save_path)
-            bf_reward = now_reward
-        
-        tq_bar.set_postfix({'lastMeanRewards': f'{now_reward:.2f}', 'BEST': f'{bf_reward:.2f}'})
-    env.close()
-    return ac_agent
-
-if __name__ == '__main__':
-    print('Training Pendulum-v1')
-    env = gym.make('Pendulum-v1')
-    cfg = Config(env)
-    ac_agent = train_agent(env, cfg)
---
+++ Q1/train_PPO.py
@@ -0,0 +1,62 @@
+import torch
+import numpy as np
+import gymnasium as gym
+from tqdm import tqdm
+
+from PPO import PPO, replayBuffer
+
+
+def main(env, cfg):
+    env = gym.make('Pendulum-v1')
+
+    # save_dir = Path("checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    # save_dir.mkdir(parents=True)
+
+    agent = PPO(
+        state_dim=env.observation_space.shape[0],
+        hidden_layers_dim=[64, 64, 64],
+        action_dim=env.action_space.shape[0],
+        actor_lr=1e-4,
+        critic_lr=5e-3,
+        gamma=0.9,
+        PPO_kwargs={'lmbda': 0.9, 'eps': 0.2, 'ppo_epochs': 10},
+        device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    )
+    buffer_size = 100000
+
+    episodes = 1200
+    max_steps = 260
+    
+    rewards = []
+    avg_reward = 0
+
+    tq_bar = tqdm(range(episodes))
+    for i in tq_bar:
+        buffer = replayBuffer(buffer_size)
+        tq_bar.set_description(f'Episode [ {i+1} / {episodes} ]')    
+        state, _ = env.reset()
+        done = False
+        episode_reward = 0
+        steps = 0
+
+        while not done:
+            action = agent.policy(state)
+            next_state, reward, done, _, _ = env.step(action)
+            buffer.add(state, action, reward, next_state, done)
+            state = next_state
+            episode_reward += reward
+            steps += 1
+            if steps >= max_steps:
+                break
+
+        agent.update(buffer.buffer)
+        rewards.append(episode_reward)
+        avg_reward = np.mean(rewards[-10:])
+
+        if (i % 20 == 0) or (i == episodes - 1):
+            torch.save(agent.actor.state_dict(), "PPO_ckpt.pth")
+        tq_bar.set_postfix({'MeanRewards': f'{avg_reward:.2f}'})
+    env.close()
+
+if __name__ == '__main__':
+    main()
