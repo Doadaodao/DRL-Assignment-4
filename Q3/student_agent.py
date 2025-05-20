f4a4af2322b2232e7384269412435380a328e033
Doadaodao <81339151+Doadaodao@users.noreply.github.com>
2025-05-20 15:47:49 +0800

    test

--- Q3/student_agent.py
+++ Q3/student_agent.py
@@ -1,7 +1,8 @@
 import gymnasium as gym
 import torch
 import numpy as np
-from DDPG import DDPG, make_env
+from sac import SAC
+import argparse
 
 import os
 import sys
@@ -9,34 +10,60 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dmc import make_dmc_env
 
+
+
 # Do not modify the input of the 'act' function and the '__init__' function. 
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
-        self.action_space = gym.spaces.Box(-1.0, 1.0, (21,), np.float64)
+        def make_env():
+            # Create environment with state observations
+            env_name = "humanoid-walk"
+            env = make_dmc_env(env_name, np.random.randint(0, 1000000), flatten=True, use_pixels=False)
+            return env
         env = make_env()
-        self.agent = DDPG(state_dim=env.observation_space.shape[0],
-                 hidden_dim=256,
-                 action_dim=env.action_space.shape[0],
-                 actor_lr=3e-4,
-                 critic_lr=3e-3,
-                 gamma=0.99,
-                 action_bound=env.action_space.high[0],
-                 sigma=0.01,
-                 tau=0.005,
-                 buffer_size=10000,
-                 minimal_size=1000,
-                 batch_size=64,
-                 device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
-                 env=env,
-                 save_dir="./checkpoints",
-                 save_interval=1000
-                 )
-        
-        # best = 65
-        self.agent.load_model("./checkpoints/2025-05-12T14-29-14/mario_net_105.chkpt")
 
+        parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
+        parser.add_argument('--env-name', default="HalfCheetah-v2",
+                            help='Mujoco Gym environment (default: HalfCheetah-v2)')
+        parser.add_argument('--policy', default="Gaussian",
+                            help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
+        parser.add_argument('--eval', type=bool, default=True,
+                            help='Evaluates a policy a policy every 10 episode (default: True)')
+        parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                            help='discount factor for reward (default: 0.99)')
+        parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                            help='target smoothing coefficient(τ) (default: 0.005)')
+        parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
+                            help='learning rate (default: 0.0003)')
+        parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
+                            help='Temperature parameter α determines the relative importance of the entropy\
+                                    term against the reward (default: 0.2)')
+        parser.add_argument('--automatic_entropy_tuning', type=bool, default=True, metavar='G',
+                            help='Automaically adjust α (default: False)')
+        parser.add_argument('--seed', type=int, default=123456, metavar='N',
+                            help='random seed (default: 123456)')
+        parser.add_argument('--batch_size', type=int, default=256, metavar='N',
+                            help='batch size (default: 256)')
+        parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
+                            help='maximum number of steps (default: 1000000)')
+        parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
+                            help='hidden size (default: 256)')
+        parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
+                            help='model updates per simulator step (default: 1)')
+        parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
+                            help='Steps sampling random actions (default: 0)')
+        parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
+                            help='Value target update per no. of updates per step (default: 1)')
+        parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
+                            help='size of replay buffer (default: 10000000)')
+        parser.add_argument('--cuda', action="store_true",
+                            help='run on CUDA (default: False)')
+        args = parser.parse_args()
+        
+        self.agent = SAC(env.observation_space.shape[0], env.action_space, args)
+        
+        self.agent.load_checkpoint("./sac_checkpoint")
 
     def act(self, observation):
-        return self.agent.act_without_noise(observation)
-        # return self.action_space.sample()
+        return self.agent.select_action(observation, evaluate=False)
